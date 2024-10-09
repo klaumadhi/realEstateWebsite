e8b1c590e1a1c3317958c17b95d4f61c2a0d8a3e
Klaurent <klaurent.madhi@uni-weimar.com>
2024-10-10 01:12:02 +0200

    newtlify fix

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -21,6 +21,7 @@ export default function Navbar() {
         <div className="right">
 
         {user ? <div className="user">
+          <Link></Link>
           <img src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
           <span>John Doe</span>
           <Link to='/profile' className="profile" >
